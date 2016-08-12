2ead06d42230c4948e37bb666ddc6243a1b39dc1
adebnath1989@gmail.com <adebnath1989@gmail.com>
2016-08-12 11:02:59 +0530

    added basic clustering script

---
+++ clustering/node-cluster-basic.js
@@ -0,0 +1,28 @@
+// Clustering basic example
+
+var cluster = require('cluster');  
+var http    = require('http');  
+var os      = require('os');
+
+var numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {  
+  // Master:
+  // Let's fork as many workers as you have CPU cores
+
+  for (var i = 0; i < numCPUs; ++i) {
+  	console.log("forking workers")
+    cluster.fork();
+  }
+} else {
+  // Worker:
+  // Let's spawn a HTTP server
+  // (Workers can share any TCP connection.
+  //  In this case its a HTTP server)
+
+  http.createServer(function(req, res) {
+    res.writeHead(200);
+    // Sends the pid of the worker that is serving the request
+    res.end('process ' + process.pid + ' says hello!'); 
+  }).listen(8080);
+}
